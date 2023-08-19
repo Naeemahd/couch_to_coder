94321217bf0249bcc5805e8d99b3491bc012a3c0
Naeemahd <98668745+Naeemahd@users.noreply.github.com>
2023-08-19 19:58:25 +0100

    Add files via upload

---
+++ long_rivers.py
@@ -0,0 +1,9 @@
+rivers = [
+ {"name": "Nile", "length": 4157},
+ {"name": "Yangtze", "length": 3434},
+ {"name": "Murray-Darling", "length": 2310},
+ {"name": "Volga", "length": 2290},
+ {"name": "Mississippi", "length": 2540},
+ {"name": "Amazon", "length": 3915}
+]
+
