d11417c90621b38bb6190e978c06f5d1acc1fc70
Naeemahd <nd493@cam.ac.uk>
2023-08-22 19:51:40 +0100

    river name loop

--- long_rivers.py
+++ long_rivers.py
@@ -1,10 +1,11 @@
 rivers = [
- {"name": "Nile", "length": 4157},
- {"name": "Yangtze", "length": 3434},
- {"name": "Murray-Darling", "length": 2310},
- {"name": "Volga", "length": 2290},
- {"name": "Mississippi", "length": 2540},
- {"name": "Amazon", "length": 3915}
-]
-#comment
+    {"name": "Nile", "length": 4157},
+    {"name": "Yangtze", "length": 3434},
+    {"name": "Murray-Darling", "length": 2310},
+    {"name": "Volga", "length": 2290},
+    {"name": "Mississippi", "length": 2540},
+    {"name": "Amazon", "length": 3915}
+    ]
 
+for river in rivers:
+    print(river["name"])
