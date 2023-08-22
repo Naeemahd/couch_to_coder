dcd704a5618dcd96b9a2aec6c80116a570f3eb36
Naeemahd <nd493@cam.ac.uk>
2023-08-22 20:11:38 +0100

    completed task 2

--- long_rivers.py
+++ long_rivers.py
@@ -6,6 +6,13 @@ rivers = [
     {"name": "Mississippi", "length": 2540},
     {"name": "Amazon", "length": 3915}
     ]
-
 for river in rivers:
     print(river["name"])
+
+total = 0
+for river in rivers:
+   total += int(river["length"])
+
+   if river["name"][0] == "M":
+       print(river["name"])
+print(total)   
